9f09ec1c18085b08daa44eedbde0c43240e99d2b
jinpp00 <medipin09@anver.com>
2022-06-10 23:35:18 +0900

    수정

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask("app")
+# app = Flask(__name__)
 
-@app.route("/")
-def index():
-  return render_template("index.html")
+# @app.route("/")
+# def index():
+#   return render_template("index.html")
 
-# app.run(host="0.0.0.0", port=8000)
+# # app.run(host="0.0.0.0", port=8000)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
